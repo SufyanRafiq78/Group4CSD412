8c9a11cdcd443d87ec3e406a51d84c5caca9d6d7
ayumim42009 <153568294+ayumim42009@users.noreply.github.com>
2025-12-04 19:20:18 -0800

    Update server.js

--- server/server.js
+++ server/server.js
@@ -13,7 +13,4 @@ app.get("/", (req, res) => {
   res.send("Animal Adoption API Running...");
 });
 
-app.listen(PORT, "0.0.0.0", () =>
-  console.log(`Server running on http://0.0.0.0:${PORT}`)
-);
-// app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
